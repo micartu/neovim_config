a5fef1ff57460f20d0e9dd9eb0da22b140b88fe6
Michael Artuerhof <Mihail.Artuhov@borkde.ru>
2025-12-06 16:41:45 +0300

    - added macro for inserting public at the start of the string

--- init.lua
+++ init.lua
@@ -5,6 +5,17 @@ vim.opt.relativenumber = true
 --- config rsolyn for csharp and dotnet
 vim.lsp.config("roslyn", {})
 
+------------------------
+---- CUSTOM MACROS -----
+------------------------
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+
+vim.fn.setreg("p", "^ipublic " .. esc .. "j^")
+
+------------------------
+---- SETUP PLUGINS -----
+------------------------
+
 return {
   {
     "stevearc/conform.nvim",
